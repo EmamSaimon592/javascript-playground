21151911fbfc4f223902109cfc732b791361ddd5
EmamSaimon592 <likonsaimon123@gmail.com>
2025-11-10 19:48:55 +0600

    if else switch

--- hello.js
+++ hello.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+console.log("Hello"); 
+
+let x = 10;
+let y = 20;
+console.log(x+y);
+
+
+let age = 50
+if (age <= 18) {
+   console.log("you are under 18");
+   
+} else {
+  console.log("you are over 18");
+  
+}
