ba663c8b1c73ed5c8cf34f829fb210e46dc76a5d
EmamSaimon592 <likonsaimon123@gmail.com>
2025-11-12 20:46:31 +0600

    js loop

---
+++ loop/loop.js
@@ -0,0 +1,36 @@
+// for (let i = 1; i <= 3; i++){
+//   for (let j = 1; j <= 2; j++){
+//     console.log(`i=${i}, j=${j}`);
+    
+//   }
+// }
+
+
+
+// while loop
+// let count = 1;
+// while (count <= 5) {
+//   console.log("count:", count);
+//   count++;
+  
+// }
+
+
+
+// do while loop
+// let i = 1;
+// do {
+//    console.log("Imam Hossain");
+//    i++;
+// } while (i <= 3);
+
+
+// for...of
+const fruits = ['apple', 'banana', 'mango'];
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+
+//
